771b277e00798c1287dffcfbf2cc473a9bb1f339
ydderfBackwards <34340157+ydderfBackwards@users.noreply.github.com>
2021-12-20 17:40:16 +0100

    Add files via upload

---
+++ AdventOfCode2021/Inputs/Day12.cs
@@ -0,0 +1,48 @@
+namespace AdventOfCode2021.Inputs
+{
+    class Day12
+    {
+// start-A
+
+
+//         public string testInput = @"start-A
+// start-b
+// A-c
+// A-b
+// b-d
+// A-end
+// b-end";
+
+        public string testInput = @"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+
+        public string input = @"dr-of
+start-KT
+yj-sk
+start-gb
+of-start
+IJ-end
+VT-sk
+end-sk
+VT-km
+KT-end
+IJ-of
+dr-IJ
+yj-IJ
+KT-yj
+gb-VT
+dr-yj
+VT-of
+PZ-dr
+KT-of
+KT-gb
+of-gb
+dr-sk
+dr-VT";
+    }
+}
---
+++ AdventOfCode2021/Inputs/Day17.cs
@@ -0,0 +1,10 @@
+namespace AdventOfCode2021.Inputs
+{
+    class Day17
+    {
+
+        public string testInput = @"target area: x=20..30, y=-10..-5";
+
+        public string input = @"target area: x=143..177, y=-106..-71";
+    }
+}
